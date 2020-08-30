4c401977354e27d417341019682afe4a70c6c1f4
luishernandez301990 <luishernandez301990@gmail.com>
2020-08-30 03:35:33 -0300

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -1,13 +1,9 @@
 const handleRegister = (req, res, pgdatabase, bcrypt) => {
   const {name, email, password} = req.body;
-  console.log("hanleRegistrationName" + name)
-  console.log("hanleRegistrationEmail" + email)
-  console.log("hanleRegistrationPassword" + password)
   if (!name || !email || !password) {
     return res.status(400).json("Incorrect form submission.");
   }
   const hash = bcrypt.hashSync(password);
-  console.log("hanleRegistrationHash" + hash)
   pgdatabase.transaction(trx => {
     trx.insert({
       hash: hash,
@@ -28,7 +24,7 @@ const handleRegister = (req, res, pgdatabase, bcrypt) => {
         })
     })
     .then(trx.commit)
-    .catch(trx.rollback)
+    .catch(trx.rollback, err => res.status(400).json("unable to register rollback"))
   })
     .catch(err => res.status(400).json("unable to register"))
 };
