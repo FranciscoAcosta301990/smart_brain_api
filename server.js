65853d1dc06e96bcdc960321193a4270f253aedf
luishernandez301990 <luishernandez301990@gmail.com>
2020-08-30 02:18:09 -0300

    fix

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const img = require("./controllers/img");
 const pgdatabase = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
-    database: "smart_brain"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
